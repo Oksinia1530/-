5a00044463655c3d86074a38dd3121387267b3b8
«Oksana <averinaoy@gmail.com>
2023-01-31 15:26:32 +0500

    task002

---
+++ Task002/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Add A");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Add B");
+int B = int.Parse(Console.ReadLine());
+
+if (B * B == A) 
+{
+
+    Console.WriteLine("YES");
+
+}
+else 
+{
+
+    Console.WriteLine("No");
+
+}
