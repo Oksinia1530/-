2dd2338dc0d6507c28d4e47d74cdcc40182ac519
«Oksana <averinaoy@gmail.com>
2023-02-01 00:14:45 +0500

    task005

---
+++ Task005/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+Console.WriteLine ("Valitse kolminumeroinen numero");
+int num = int.Parse(Console.ReadLine()!);
+int kon = num % 10;
+Console.WriteLine(kon);
