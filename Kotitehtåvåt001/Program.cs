9c08b31cbe9f2c468b1390296ea2bfe4b062dba6
«Oksana <averinaoy@gmail.com>
2023-02-01 00:35:39 +0500

    Kotitehtävä001

---
+++ Kotitehtåvåt001/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+Console.WriteLine("Lisää A");
+int A = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Lisää B");
+int B = int.Parse(Console.ReadLine()!);
+if (A > B) 
+{
+
+    Console.WriteLine($"Numero {A} on suurempi kuin {B}");
+
+}
+else 
+{
+
+    Console.WriteLine($"Numero {A} on pienempi kuin {B}");
+
+}
