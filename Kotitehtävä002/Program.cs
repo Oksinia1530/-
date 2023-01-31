7e87e9f734e3c226dc4c9c90914babd9a8f2c8aa
«Oksana <averinaoy@gmail.com>
2023-02-01 00:49:37 +0500

    Kotitehtävä002

---
+++ Kotitehtävä002/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Enter A");
+int A = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Enter B");
+int B = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Enter C");
+int C = int.Parse(Console.ReadLine()!);
+
+int max = A;
+
+if ( A > max) max = A;
+if ( B > max) max = B;
+if ( C > max) max = C;
+
+Console.Write("max= ");
+Console.WriteLine(max);
