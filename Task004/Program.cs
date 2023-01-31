44ee976bfe94651c3ff6fd82777c8472994b500f
«Oksana <averinaoy@gmail.com>
2023-02-01 00:04:43 +0500

    task004

---
+++ Task004/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+Console.WriteLine ("Lisää numero");
+int N = Math.Abs(int.Parse(Console.ReadLine()));
+int K = -N;
+while (K<=N)
+{
+    Console.Write($"{K} ");
+    K++;
+}
