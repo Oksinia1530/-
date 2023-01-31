81f49486156416f4fd5cf295dcd3170babcdc838
«Oksana <averinaoy@gmail.com>
2023-01-31 23:40:34 +0500

    task003_1

---
+++ Task003/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+Console.WriteLine ("Lisää viikonpäivän numero");
+
+int num = int.Parse(Console.ReadLine());
+
+if (num == 1)
+{
+
+     Console.WriteLine("maanantai");
+}
+else if (num == 2)
+{
+
+     Console.WriteLine("tiistai");
+}
+else if (num == 3)
+{
+
+     Console.WriteLine("keskiviikko");
+}
+else if (num == 4)
+{
+
+     Console.WriteLine("torstai");
+}
