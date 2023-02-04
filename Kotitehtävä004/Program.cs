68060d9546997cab16e2c7c75aefbaf2d0b05e0d
«Oksana <averinaoy@gmail.com>
2023-02-04 11:37:12 +0500

    Kotitehtävä004

---
+++ Kotitehtävä004/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+
+Console.WriteLine("Enter A");
+int A = int.Parse(Console.ReadLine()!);
+int start = 0;
+while (start <= A)
+{
+    if (start % 2 == 0)
+    {
+        Console.Write($"{start} ");
+    }
+
+    start++;
+}
