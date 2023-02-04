841d55dac078a2b73482d80848457d8e7510186a
«Oksana <averinaoy@gmail.com>
2023-02-04 10:33:54 +0500

    Kotitehtävä003

---
+++ Kotitehtävä003/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.Clear();
+
+Console.WriteLine("Enter A");
+int A = int.Parse(Console.ReadLine()!);
+A = A % 2;
+if (A==1)
+{
+
+Console.WriteLine("Se ei ole parillinen luku");
+
+}
+else
+{
+
+    Console.WriteLine("Se on parillinen luku");
+}
